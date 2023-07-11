6dd001b3b089afcaf42eec1a66f520c7e6f1d56c
Anggi <anggiburox@gmail.com>
2023-07-11 08:04:13 +0700

    up keberangaktan role pimpinan

--- resources/views/pages/pimpinan/keberangkatan/index.blade.php
+++ resources/views/pages/pimpinan/keberangkatan/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="pagetitle">
-    <h1>Data Paket Umrah</h1>
+    <h1>Data Keberangkatan</h1>
 </div><!-- End Page Title -->
 
 <section class="section">
@@ -22,10 +22,12 @@
                         <thead>
                             <tr>
                                 <th scope="col">No</th>
-                                <th scope="col">ID Paket Umrah</th>
-                                <th scope="col">Nama Paket Umrah</th>
-                                <th scope="col">Harga Paket Umrah</th>
-                                <th scope="col">Cetak</th>
+                                <th scope="col">Kode</th>
+                                <th scope="col">NIK - Nama Jamaah</th>
+                                <th scope="col">Nama - Harga Paket Umrah</th>
+                                <th scope="col">Tanggal Keberangkatan</th>
+                                <th scope="col">Titik Kumpul</th>
+                                <th scope="col">Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -34,12 +36,16 @@
                             <?php $no++ ;?>
                             <tr>
                                 <td>{{$no}}</td>
-                                <td>{{$p->ID_Paket_Umrah}}</td>
-                                <td>{{$p->Nama_Paket_Umrah}}</td>
-                                <td>Rp. {{ number_format($p->Harga_Paket_Umrah, 0, ',', '.') }}</td>
+                                <td>{{$p->Kode_Keberangkatan}}</td>
+                                <td>{{$p->NIK}} - {{$p->Nama_Jamaah}}</td>
+                                <td>{{$p->Nama_Paket_Umrah}} - Rp.
+                                    {{ number_format(floatval($p->Harga_Paket_Umrah), 0, ',', '.') }}
+                                </td>
+                                <td>{{ \Carbon\Carbon::parse($p->Tanggal_Keberangkatan)->isoFormat('D MMMM Y') }}</td>
+                                <td>{{$p->Titik_Kumpul}}</td>
                                 <td>
                                     <a href="keberangkatan/cetak_pdf_satuan/{{ $p->Kode_Keberangkatan}}"
-                                        class="btn mb-1 btn-success btn-sm" data-toggle="tooltip" data-placement="top"
+                                        class="btn mb-1 btn-secondary btn-sm" data-toggle="tooltip" data-placement="top"
                                         title="Cetak" type="button"><i class="bi bi-printer-fill"></i>&nbsp; Cetak</a>
                                 </td>
                             </tr>
@@ -54,34 +60,4 @@
         </div>
     </div>
 </section>
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-<script>
-function showConfirmation(event) {
-    event.preventDefault();
-    var deleteLink = event.target.getAttribute('href');
-
-    swal({
-        title: "Konfirmasi",
-        text: "Apakah Anda yakin ingin menghapus data ini?",
-        icon: "warning",
-        buttons: ["Batal", "Ya"],
-        dangerMode: true,
-    }).then((confirm) => {
-        if (confirm) {
-            window.location.href = deleteLink;
-        }
-    });
-}
-</script>
-@if(Session::has('success'))
-<script>
-swal("Sukses", "{{ Session::get('success') }}", "success");
-</script>
-@endif
-
-@if(Session::has('danger'))
-<script>
-swal("Sukses", "{{ Session::get('danger') }}", "success");
-</script>
-@endif
 @endsection
